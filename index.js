a8c36a190be13048fb7d50b0d517578703f3093a
Taiye Salami <taiye41@gmail.com>
2019-05-12 05:10:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,44 @@
+function add(a, b) {
+var number = a+b
+return number
+}
+
+function subtract (a, b) {
+var  number = a-b
+  return a-b
+}
+
+function multiply (a, b) {
+  var number = a*b
+  return a*b
+}
+
+function divide (a,b) {
+  var number = a/b
+  return number
+}
+
+function inc(a) {
+  var n=a+1
+  return n
+}
+
+function dec(a) {
+  var number=a-1
+  return number
+}
+
+function makeInt(a){
+  var n = a;
+  return n
+}
+
+function preserveDecimal(n){
+  n = 2.222
+    return n;
+}
+
+function preserveDecimal(a) {
+  a = `sldkjflksjf`
+    return a
+}
